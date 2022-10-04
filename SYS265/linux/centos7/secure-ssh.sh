cdfd22153b822972fded2ace3fa0025515b0a7cf
HusseinM5 <mohammed.hussein@mymail.champlain.edu>
2022-10-04 22:35:26 +0000

    secure-ssh.sh v2

--- SYS265/linux/centos7/secure-ssh.sh
+++ SYS265/linux/centos7/secure-ssh.sh
@@ -3,11 +3,16 @@
 # You have to run the script from where the script is located at
 
 username=$1
-useradd $1
+useradd -m -d /home/$1 -s /bin/bash $1
 mkdir -p /home/$1/.ssh
 
 touch /home/$1/.ssh/authorized_keys
 cat ../public-keys/id_rsa.pub >> /home/$1/.ssh/authorized_keys
+
+chmod 700 /home/$1/.ssh
+chmod 600 /home/$1/.ssh/authorized_keys
+chown -R $1:$1 /home/$1/.ssh
+chown -R $1:$1 /home/$1
 
 sed -i 's/#PermitRootLogin/PermitRootLogin/g' /etc/ssh/sshd_config
 sed -i '/PermitRootLogin/s/yes/no/g' /etc/ssh/sshd_config
